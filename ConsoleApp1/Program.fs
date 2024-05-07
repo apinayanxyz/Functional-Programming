3fb5619bc5eb338c34802b062695df78eec8cfca
apinayanxyz <akanenthirarasa@gmail.com>
2024-05-08 00:54:14 +0100

    finished task 3

--- ConsoleApp1/Program.fs
+++ ConsoleApp1/Program.fs
@@ -5,8 +5,12 @@ printfn "Simple banking system"
 //Task 1
 type Account = {AccountNumber:string; 
                 mutable Balance:float;} 
-                member this.Withdraw x = this.Balance - x
-                member this.Deposit x = this.Balance + x
+                member this.Withdraw x = 
+                    Console.WriteLine("|| " + x.ToString() + " has been taken out of -> Account number : " + this.AccountNumber + " || Balance : " + this.Balance.ToString() + " ||")
+                    this.Balance - x
+                member this.Deposit x = 
+                    Console.WriteLine("|| " + x.ToString() + " has been added in to -> Account number : " + this.AccountNumber + " || Balance : " + this.Balance.ToString() + " ||")
+                    this.Balance + x
                 member this.Print =
                     Console.WriteLine("|| Account number : " + this.AccountNumber + " || Balance : " + this.Balance.ToString() + " ||") 
 
@@ -14,9 +18,12 @@ type Account = {AccountNumber:string;
 
 let AccountCopy account = 
     match account with
-    |_ when account.Balance < 10.0 -> Console.WriteLine("Balance is low")
-    |_ when account.Balance >= 10.0 && account.Balance<=100 -> Console.WriteLine("Balance is ok")
-    |_ when account.Balance > 100.0 -> Console.WriteLine("Balance is high")
+    |_ when account.Balance < 10.0 -> 
+        Console.WriteLine("Balance is low")
+    |_ when account.Balance >= 10.0 && account.Balance<=100 -> 
+        Console.WriteLine("Balance is ok")
+    |_ when account.Balance > 100.0 -> 
+        Console.WriteLine("Balance is high")
 
 let account1 = {AccountNumber = "0001"; Balance = 00.0}
 account1.Print
@@ -36,3 +43,40 @@ AccountCopy account5
 let account6 = {AccountNumber = "0006"; Balance = 00.5}
 account6.Print
 AccountCopy account6
+
+//Task 3
+let accountList  = [
+    account1;
+    account2;
+    account3;
+    account4;
+    account5;
+    account6;
+]
+let lowSequence = seq{
+        for i in accountList do
+            if i.Balance >= 0 then
+                if i.Balance < 50 then
+                    i
+}
+
+let highSequence = seq{
+    for i in accountList do
+        if i.Balance >50 then
+            i
+}
+
+Console.WriteLine("List of accounts whose balance falls between 0 and 50")
+for i in lowSequence do
+    i.Print
+
+Console.WriteLine("List of accounts whose balance exceeds 50")
+for i in highSequence do
+    i.Print
+
+//Task 4
+type Ticket = {seat:int; customer:string}
+
+let mutable tickets = [for n in 1..10 -> {Ticket.seat = n; Ticket.customer = ""}]
+
+
